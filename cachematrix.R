78d1d59ac19b2b576417a972a12144a961584f86
Biswajit Khandai <b_khandai@yahoo.com>
2017-10-29 20:26:04 -0700

    camelCasing for names.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
-## makeCacheMatrix creates a special Matrix object that can cache
+## makeCacheMatrix() creates a special Matrix object that can cache
 ## its inverse. it defines some get and set functions for the matrix
 ## object, and returns a list of these functions.
 
 makeCacheMatrix <- function(x = matrix()) {
-    inv_mtx  <- NULL
+    inv_mtx  <- NULL      ## Initialize inverted matrix as NULL
 
     set <- function(y) {
         x       <<- y
@@ -14,19 +14,19 @@ makeCacheMatrix <- function(x = matrix()) {
         x
     }
 
-    setinv <- function(im) {
-        inv_mtx <<- im  ## Cache the inverse value
+    setInv <- function(im) {
+        inv_mtx <<- im    ## Cache the inverse value
     }
 
-    getinv <- function() {
+    getInv <- function() {
         inv_mtx
     }
 
-    list (set = set, get = get, setinv = setinv, getinv = getinv)
+    list (set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## cacheSolve returns the inverse of a matrix. When the inverse of this matrix
+## cacheSolve() returns the inverse of a matrix. When the inverse of this matrix
 ## has not been computed earlier, cacheSolve computes the inverse and returns
 ## it, but also caches it for future use. If it has been computed already once,
 ## cacheSolve returns the cached value.
@@ -34,10 +34,10 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     
-    inv_mtx <- x$getinv()
+    inv_mtx <- x$getInv()
 
     if (!is.null(inv_mtx)) {
-        ## Cached value is non-NULL, hence return cached value
+                  ## Cached value is non-NULL, hence return cached value
         message ("getting cached data")
         return (inv_mtx)
     }
@@ -48,7 +48,7 @@ cacheSolve <- function(x, ...) {
 
     data      <- x$get()
     inv_mtx   <- solve(data, ...)
-    x$setinv (inv_mtx)  ## This function call will do the actual caching
+    x$setInv (inv_mtx)    ## This function call will do the actual caching
 
     inv_mtx
 }
